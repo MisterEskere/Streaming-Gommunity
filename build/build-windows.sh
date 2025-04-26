3c7384508d3b6ea703b64749f97e2595256fa33e
mistereskere <no@thank.you>
2025-04-26 12:07:22 +0200

    fixed build

--- build/build-windows.sh
+++ build/build-windows.sh
@@ -1,3 +1,4 @@
 echo "Building for Windows..."
+cd "$(dirname "$0")/.." # Navigate to the project root
 GOOS=windows GOARCH=amd64 go build -o build/Streaming-Gommunity.exe ./src/main.go
 echo "Build completed: build/Streaming-Gommunity.exe"
